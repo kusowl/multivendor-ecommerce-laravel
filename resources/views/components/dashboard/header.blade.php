51ec67e1a98283c786bb0f25e0790d3f819267e9
kusowl <kushal.saha@example.com>
2025-10-19 20:41:43 +0530

    fix: dropdown item color

--- resources/views/components/dashboard/header.blade.php
+++ resources/views/components/dashboard/header.blade.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Dashboard</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link href="{{ asset('vendor/bladewind/css/animate.min.css') }}" rel="stylesheet"/>
     <link href="{{ asset('vendor/bladewind/css/bladewind-ui.min.css') }}" rel="stylesheet"/>
     <script src="{{ asset('vendor/bladewind/js/helpers.js') }}"></script>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
 </head>
 <body>
