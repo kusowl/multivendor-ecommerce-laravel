741d9fdc28eb780a09848513c42707dc9d282211
kusowl <kushal.saha@example.com>
2025-10-30 18:46:00 +0530

    feat: product page for store

---
+++ resources/views/store/product.blade.php
@@ -0,0 +1,206 @@
+<x-layouts.store>
+    <x-store.page-header page="{{$product->name}}"/>
+    <section class="single-product">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-5">
+                    <div class="single-product-slider">
+                        <div id='carousel-custom' class='carousel slide' data-ride='carousel'>
+                            <div class='carousel-outer'>
+                                <!-- me art lab slider -->
+                                <div class='carousel-inner '>
+                                    @foreach(explode(',', $product->image) as $index => $image)
+                                        <div class='item {{$index == 0 ? 'active': ''}}'>
+                                            <img src='{{\App\Utils\File::getImage($image)}}' alt=''
+                                                 data-zoom-image="{{\App\Utils\File::getImage($image)}}"
+                                            />
+                                        </div>
+                                    @endforeach
+                                </div>
+
+                                <!-- sag sol -->
+                                <a class='left carousel-control' href='#carousel-custom' data-slide='prev'>
+                                    <i class="tf-ion-ios-arrow-left"></i>
+                                </a>
+                                <a class='right carousel-control' href='#carousel-custom' data-slide='next'>
+                                    <i class="tf-ion-ios-arrow-right"></i>
+                                </a>
+                            </div>
+
+                            <!-- thumb -->
+                            <ol class='carousel-indicators mCustomScrollbar meartlab'>
+                                @foreach(explode(',', $product->image) as $index => $image)
+                                    <li data-target='#carousel-custom' data-slide-to='{{$index}}'
+                                        class='{{$index == 0 ? 'active' : ''}}'>
+                                        <img src='{{\App\Utils\File::getImage($image)}}' alt=''
+                                             data-zoom-image="{{\App\Utils\File::getImage($image)}}"
+                                        />
+                                    </li>
+                                @endforeach
+
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-7">
+                    <div class="single-product-details">
+                        <div class="">
+                        </div>
+                        <div class="color-swatches">
+                            <span>color:</span>
+                            <ul>
+                                <li>
+                                    <a href="#!" class="swatch-violet"></a>
+                                </li>
+                                <li>
+                                    <a href="#!" class="swatch-black"></a>
+                                </li>
+                                <li>
+                                    <a href="#!" class="swatch-cream"></a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="product-size">
+                            <span>Size:</span>
+                            <select class="form-control">
+                                <option>S</option>
+                                <option>M</option>
+                                <option>L</option>
+                                <option>XL</option>
+                            </select>
+                        </div>
+                        <div class="product-quantity">
+                            <span>Quantity:</span>
+                            <div class="product-quantity-slider">
+                                <input id="product-quantity" type="text" value="0" name="product-quantity">
+                            </div>
+                        </div>
+                        <div class="product-category">
+                            <span>Categories:</span>
+                            <ul>
+                                @foreach($product->category() as $category)
+                                    <li><a href="product-single.html">{{$category->name}}</a></li>
+                                @endforeach
+                            </ul>
+                        </div>
+                        <a href="cart.html" class="btn btn-main mt-20">Add To Cart</a>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="tabCommon mt-20">
+                        <ul class="nav nav-tabs">
+                            <li class="active"><a data-toggle="tab" href="#details" aria-expanded="true">Details</a>
+                            </li>
+                            <li class=""><a data-toggle="tab" href="#reviews" aria-expanded="false">Reviews (3)</a></li>
+                        </ul>
+                        <div class="tab-content patternbg">
+                            <div id="details" class="tab-pane fade active in">
+                                <h4>Product Description</h4>
+                                {!! $product->description !!}
+                            </div>
+                            <div id="reviews" class="tab-pane fade">
+                                <div class="post-comments">
+                                    <ul class="media-list comments-list m-bot-50 clearlist">
+                                        <!-- Comment Item start-->
+                                        <li class="media">
+
+                                            <a class="pull-left" href="#!">
+                                                <img class="media-object comment-avatar" src="images/blog/avater-1.jpg"
+                                                     alt="" width="50" height="50"/>
+                                            </a>
+
+                                            <div class="media-body">
+                                                <div class="comment-info">
+                                                    <h4 class="comment-author">
+                                                        <a href="#!">Jonathon Andrew</a>
+
+                                                    </h4>
+                                                    <time datetime="2013-04-06T13:53">July 02, 2015, at 11:34</time>
+                                                    <a class="comment-button" href="#!"><i
+                                                            class="tf-ion-chatbubbles"></i>Reply</a>
+                                                </div>
+
+                                                <p>
+                                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at
+                                                    magna ut ante eleifend eleifend.Lorem ipsum dolor sit amet,
+                                                    consectetur
+                                                    adipisicing elit. Quod laborum minima, reprehenderit laboriosam
+                                                    officiis
+                                                    praesentium? Impedit minus provident assumenda quae.
+                                                </p>
+                                            </div>
+
+                                        </li>
+                                        <!-- End Comment Item -->
+
+                                        <!-- Comment Item start-->
+                                        <li class="media">
+
+                                            <a class="pull-left" href="#!">
+                                                <img class="media-object comment-avatar" src="images/blog/avater-4.jpg"
+                                                     alt="" width="50" height="50"/>
+                                            </a>
+
+                                            <div class="media-body">
+
+                                                <div class="comment-info">
+                                                    <div class="comment-author">
+                                                        <a href="#!">Jonathon Andrew</a>
+                                                    </div>
+                                                    <time datetime="2013-04-06T13:53">July 02, 2015, at 11:34</time>
+                                                    <a class="comment-button" href="#!"><i
+                                                            class="tf-ion-chatbubbles"></i>Reply</a>
+                                                </div>
+
+                                                <p>
+                                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at
+                                                    magna ut ante eleifend eleifend. Lorem ipsum dolor sit amet,
+                                                    consectetur
+                                                    adipisicing elit. Magni natus, nostrum iste non delectus atque ab a
+                                                    accusantium optio, dolor!
+                                                </p>
+
+                                            </div>
+
+                                        </li>
+                                        <!-- End Comment Item -->
+
+                                        <!-- Comment Item start-->
+                                        <li class="media">
+
+                                            <a class="pull-left" href="#!">
+                                                <img class="media-object comment-avatar" src="images/blog/avater-1.jpg"
+                                                     alt="" width="50" height="50">
+                                            </a>
+
+                                            <div class="media-body">
+
+                                                <div class="comment-info">
+                                                    <div class="comment-author">
+                                                        <a href="#!">Jonathon Andrew</a>
+                                                    </div>
+                                                    <time datetime="2013-04-06T13:53">July 02, 2015, at 11:34</time>
+                                                    <a class="comment-button" href="#!"><i
+                                                            class="tf-ion-chatbubbles"></i>Reply</a>
+                                                </div>
+
+                                                <p>
+                                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at
+                                                    magna ut ante eleifend eleifend.
+                                                </p>
+
+                                            </div>
+
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</x-layouts.store>
