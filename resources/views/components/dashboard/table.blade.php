1394c29dc953a2fa66a08fef96dd9c2b9279b604
kusowl <kushal.saha@example.com>
2025-11-03 08:43:54 +0530

    add view url logic in table

--- resources/views/components/dashboard/table.blade.php
+++ resources/views/components/dashboard/table.blade.php
@@ -4,16 +4,23 @@
     'modal_title' => '',
     'delete_route' => '',
     'edit_route' => '',
+    'view_route' => '',
     'route_key' => '',
     'exclude_columns' => ''
 ])
 @php
     if($action_icons === []){
         $key = str_replace(':','',$route_key);
-        $action_icons = [
-            "icon:pencil | tip:edit | click:editItem('{{$key}}')",
-            "icon:trash | color:red | tip:delete | click:showModal('delete-item', {slug: '{{$key}}'})",
-        ];
+        $action_icons = [];
+        if($edit_route !== ''){
+            $action_icons[] = "icon:pencil | tip:edit | click:editItem('{{$key}}')";
+        }
+        if($view_route !== ''){
+        $action_icons[] = "icon:eye | tip:view | click:viewItem('{{$key}}')";
+        }
+        if($delete_route !== ''){
+            $action_icons[]  = "icon:trash | color:red | tip:delete | click:showModal('delete-item', {slug: '{{$key}}'})";
+        }
     }
 @endphp
 <div class="w-full">
@@ -34,5 +41,9 @@
     const editUrl = @json($edit_route);
     const editItem = (value) => {
         window.location.href = editUrl.replace(@json($route_key), value);
+    }
+    const viewUrl = @json($view_route);
+    const viewItem = (value) => {
+        window.open(viewUrl.replace(@json($route_key), value), "_blank");
     }
 </script>
