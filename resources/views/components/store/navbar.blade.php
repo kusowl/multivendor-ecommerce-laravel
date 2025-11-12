28879209269110e8b6620a65da1dcca02f82ec24
kusowl <kushal.saha@outlook.com>
2025-11-12 09:43:07 +0530

    add navbar

--- resources/views/components/store/navbar.blade.php
+++ resources/views/components/store/navbar.blade.php
@@ -1,56 +1,77 @@
-@props(['categories'])
+<nav class="bg-base-100 border-b border-b-base-300 shadow-lg shadow-base-content/5 sticky top-0 z-50">
+    <div class="navbar mx-auto w-10/12">
+        <div class="flex-1 flex gap-2">
+            <a href="/" class="btn btn-ghost text-xl">{{config('app.name')}}</a>
+            <form action="" method="get" class="flex flex-1">
+                <label class="input flex-1 mr-12">
+                    <i data-lucide="search"></i>
+                    <input type="text" placeholder="Search for Products, Categories and more"
+                           class="input input-bordered "/>
+                </label>
+            </form>
+        </div>
+        <div class="flex gap-2">
+            @guest
+                <a href="{{route('login')}}" tabindex="0" role="button" class="btn btn-soft ">
+                    <div class="flex gap-2  items-center">
+                        <i data-lucide="scan-face"></i>
+                        Login
+                    </div>
+                </a>
+            @endguest
+            @auth
+                <a href="{{route('cart.index')}}" tabindex="0" role="button" class="btn btn-ghost btn-circle">
+                    <div class="indicator">
+                        <i data-lucide="shopping-cart"></i>
+                        <span class="badge badge-sm indicator-item">8</span>
+                    </div>
+                </a>
 
-<section class="menu">
-    <nav class="navbar navigation">
-        <div class="container">
-            <div class="navbar-header">
-                <h2 class="menu-title">Menu</h2>
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
-                        aria-expanded="false" aria-controls="navbar">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-
-            </div>
-
-            <!-- Navbar Links -->
-            <div id="navbar" class="navbar-collapse collapse text-center">
-                <ul class="nav navbar-nav">
-
-                    <li class="dropdown ">
-                        <a href="{{route('home')}}">Home</a>
-                    </li>
-
-                    <!-- Pages -->
-                    <li class="dropdown dropdown-slide">
-                        <a href="#!" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown"
-                           data-delay="350"
-                           role="button" aria-haspopup="true" aria-expanded="false">Categories <span
-                                class="tf-ion-ios-arrow-down"></span></a>
-                        <div class="dropdown-menu ">
-                            <div class="row">
-                                @foreach($categories as $category)
-                                    <div class="col-sm-4">
-                                        <ul>
-                                            <li class="dropdown-header">{{$category->name}}</li>
-                                            <li role="separator" class="divider"></li>
-                                            @foreach($category->subCategories()->latest()->take(4)->get() as $subCategories)
-                                                <li><a href="{{route('store.categories.subCategories.show', [$category, $subCategories->slug])}}">{{$subCategories->name}}</a></li>
-                                            @endforeach
-                                        </ul>
-                                    </div>
-                                @endforeach
-                                <div class="col-sm-12">
-                                    <a href="{{route('store.categories')}}" class="btn btn-main btn-medium col-sm-12">View
-                                        all</a>
-                                </div>
+                <div class="dropdown dropdown-end">
+                    <div tabindex="0" role="button" class="btn btn-ghost">
+                        <div class="avatar">
+                            <div class="w-10 rounded-full">
+                                <img
+                                    alt="Tailwind CSS Navbar component"
+                                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"/>
                             </div>
-                        </div><!-- / .dropdown-menu -->
-                    </li><!-- / Pages -->
+                        </div>
+                        {{auth()->user()->name}}
+                    </div>
+
+                    <ul
+                        tabindex="-1"
+                        class="menu menu-lg md:menu-md dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                        <li>
+                            <a class="justify-between">
+                                Profile
+                                <span class="badge">New</span>
+                            </a>
+                        </li>
+                        <li><a>Settings</a></li>
+                        <li>
+                            <form action="{{route('logout')}}" method="post">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit">Logout</button>
+                            </form>
+                        </li>
+                    </ul>
+                </div>
+            @endauth
+            <div class="dropdown">
+                <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+                    <i data-lucide="ellipsis-vertical"></i>
+                </div>
+                <ul
+                    tabindex="-1"
+                    class="menu menu-md dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                    <li><a><i data-lucide="headset"></i>Helpdesk</a></li>
+                    <li><a><i data-lucide="user-pen"></i>Contact Us</a></li>
+                    <li><a><i data-lucide="users-round"></i>About</a></li>
+                </ul>
             </div>
-            <!--/.navbar-collapse -->
-        </div><!-- / .container -->
-    </nav>
-</section>
+        </div>
+    </div>
+    </div>
+</nav>
