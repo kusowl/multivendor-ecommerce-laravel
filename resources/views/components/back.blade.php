54da43cf66d8006d47f4531692c2a8170e7ce1df
kusowl <kushal.saha@outlook.com>
2025-12-01 00:29:58 +0530

    fix go back - use browser history API

--- resources/views/components/back.blade.php
+++ resources/views/components/back.blade.php
@@ -1,3 +1,3 @@
-<a href="{{url()->previous()}}" class="btn btn-dash border-gray-400 text-gray-500">
+<button onclick="history.back()" class="btn btn-dash border-gray-400 text-gray-500">
     <i data-lucide="arrow-left"></i> Back
-</a>
+</button>
